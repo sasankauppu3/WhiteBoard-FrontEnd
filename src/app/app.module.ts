1b470b57f09d6c5df6549f36458f10814f648351
Veera Venkata Sasanka Uppu <uppu.v@husky.neu.edu>
2018-11-17 17:38:49 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseGridComponentComponent } from './course-grid-component/course-grid-component.component';
+import { ModuleListComponentComponent } from './module-list-component/module-list-component.component';
+import { LessonTabsComponentComponent } from './lesson-tabs-component/lesson-tabs-component.component';
+import { TopicPillsComponentComponent } from './topic-pills-component/topic-pills-component.component';
+import { WidgetListComponentComponent } from './widget-list-component/widget-list-component.component';
+import {CourseServiceClient} from './services/CourseServiceClient';
+import {ModuleServiceClient} from './services/ModuleServiceClient';
+import {LessonServiceClient} from './services/LessonServiceClient';
+import {TopicServiceClient} from './services/TopicServiceClient';
+import {WidgetServiceClient} from './services/WidgetServiceClient';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseGridComponentComponent,
+    ModuleListComponentComponent,
+    LessonTabsComponentComponent,
+    TopicPillsComponentComponent,
+    WidgetListComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CourseServiceClient,
+    LessonServiceClient,
+    ModuleServiceClient,
+    TopicServiceClient,
+    WidgetServiceClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/course-grid-component/course-grid-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-grid-component',
+  templateUrl: './course-grid-component.component.html',
+  styleUrls: ['./course-grid-component.component.css']
+})
+export class CourseGridComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
